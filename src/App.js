c76cec69292c9fd68dbb4369d748fb3253ca1a4e
Fatimah Patel <fatimahpatel10@gmail.com>
2024-03-12 12:47:27 +0000

    import AudioMinRow.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import PriceRow from './components/PriceRow'
+import AudioMinsRow from './components/AudioMinsRow';
 
 function App() {
   return (
     <>
       <h1>AAT price converter</h1>
+      <AudioMinsRow />
+      <div>=</div>
       <PriceRow />
     </>   
   );
---
+++ src/components/AudioMinsRow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AudioMinsRow = () => {
+  return (
+    <div>
+      Audio mins row
+    </div>
+  )
+}
+
+export default AudioMinsRow;
